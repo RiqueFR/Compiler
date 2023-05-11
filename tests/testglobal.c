26d0852ad2d309a4d5027c7e4b40b8a4586ea34c
leol0ps <115355675+leol0ps@users.noreply.github.com>
2023-05-11 16:05:53 -0300

    test same name declaration

---
+++ tests/testglobal.c
@@ -0,0 +1,14 @@
+
+int x = 15;
+
+void test(int x){
+    int test = 5;
+    //printf("test %d\n", test);
+}
+int main(){
+    test(x);
+    float x = 10;
+    //int x = 11;
+    //printf("%lf\n",x);
+    return 0;
+}
